06bc96cdcfae4fe38a25ccc80a0b7e7a1162a173
Lorenzzo <lorenzzoleifeld@gmail.com>
2026-04-18 12:34:19 -0300

    alteração em nome de váriavel

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const botao = document.querySelector(".btn");
 const entrada = document.querySelector(".input");
-const lista = document.getElementById("lista")
+const lista = document.querySelector(".lista")
 botao.addEventListener("click", () => {
     if(entrada.value === "")
         return;
-    const novoItem = document.createElement("li"); alert("Você criou um novo elemento")
+    const novoItem = document.createElement("li");
     novoItem.textContent = entrada.value;
     lista.appendChild(novoItem);
     novoItem.addEventListener("click", () => {
         if(novoItem.classList.contains("estilos")) {
             lista.removeChild(novoItem);
-            alert("Você removeu seu novo elemento!")
         } else {
             novoItem.classList.add("estilos");
         }
-    })
-})
+        })
+        })
